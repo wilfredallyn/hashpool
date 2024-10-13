e65d6cb8b3a058480f82d9cb69cff2914c52c2c1
Godefroid Chapelle <gotcha@bubblenet.be>
2024-10-13 10:37:10 +0200

    isolate bitcoind data

--- devenv.nix
+++ devenv.nix
@@ -24,6 +24,9 @@ let
   ];
   });
 in {
+
+  env.BITCOIND_DATADIR = config.devenv.root + "/.devenv/state/bitcoind";
+
   # https://devenv.sh/packages/
   packages = [ bitcoind minerd ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.Security ];
 
@@ -45,11 +48,11 @@ in {
   # services.postgres.enable = true;
   # https://devenv.sh/scripts/
   scripts = {
-     run-local-pool.exec = "cargo -C roles/pool -Z unstable-options run -- -c $(pwd)/roles/pool/config-examples/pool-config-local-tp-example.toml";
-     run-job-server.exec = "cargo -C roles/jd-server -Z unstable-options run -- -c $(pwd)/roles/jd-server/config-examples/jds-config-local-example.toml";
-     run-job-client.exec = "cargo -C roles/jd-client -Z unstable-options run -- -c $(pwd)/roles/jd-client/config-examples/jdc-config-local-example.toml";
-     run-translator-proxy.exec = "cargo -C roles/translator -Z unstable-options run -- -c $(pwd)/roles/translator/config-examples/tproxy-config-local-jdc-example.toml";
-     bitcoind-testnet.exec = "bitcoind -testnet4 -sv2 -sv2port=8442 -debug=sv2 -conf=$(pwd)/bitcoin.conf";
+     run-local-pool.exec = "cargo -C roles/pool -Z unstable-options run -- -c $DEVENV_ROOT/roles/pool/config-examples/pool-config-local-tp-example.toml";
+     run-job-server.exec = "cargo -C roles/jd-server -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-server/config-examples/jds-config-local-example.toml";
+     run-job-client.exec = "cargo -C roles/jd-client -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-client/config-examples/jdc-config-local-example.toml";
+     run-translator-proxy.exec = "cargo -C roles/translator -Z unstable-options run -- -c $DEVENV_ROOT/roles/translator/config-examples/tproxy-config-local-jdc-example.toml";
+     bitcoind-testnet.exec = "bitcoind -testnet4 -sv2 -sv2port=8442 -debug=sv2 -conf=$DEVENV_ROOT/bitcoin.conf -datadir=$BITCOIND_DATADIR";
      run-minerd.exec = "minerd -a sha256d -o stratum+tcp://localhost:34255 -q -D -P";
   };
 
@@ -57,4 +60,9 @@ in {
   # https://devenv.sh/tests/
   # https://devenv.sh/pre-commit-hooks/
   # See full reference at https://devenv.sh/reference/options/
+
+  tasks."bitcoind:make_datadir" = {
+    exec = ''mkdir -p $BITCOIND_DATADIR'';
+    before = [ "devenv:enterShell" ];
+  };
 }
