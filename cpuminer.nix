3033e69832acb1f7d2821e2000b6e37878ebf77d
Godefroid Chapelle <gotcha@bubblenet.be>
2024-10-13 11:27:36 +0200

    install pre-commit nix formatter

--- cpuminer.nix
+++ cpuminer.nix
@@ -1,29 +1,31 @@
-{ pkgs, ...}:
-let
+{pkgs, ...}: let
   src = pkgs.fetchFromGitHub {
     owner = "pooler";
     repo = "cpuminer";
     rev = "v2.5.1";
     hash = "sha256-ERBcFKAWsNW2UbqAfEaRsgIMcBfp+ggFA6LFjD6IhDg=";
   };
-  configurePhase = if pkgs.stdenv.isDarwin then ''
-    ./autogen.sh
-    ./configure CFLAGS="-O3" --disable-assembly
-  ''
-  else ''
-    ./autogen.sh
-    ./configure CFLAGS="-O3"
-  '';
-in pkgs.stdenv.mkDerivation {
-  name = "cpuminer";
-  src = src;
-  buildInputs = [ pkgs.autoconf pkgs.automake pkgs.curl ];
-  configurePhase = configurePhase;
-  buildPhase = ''
+  configurePhase =
+    if pkgs.stdenv.isDarwin
+    then ''
+      ./autogen.sh
+      ./configure CFLAGS="-O3" --disable-assembly
+    ''
+    else ''
+      ./autogen.sh
+      ./configure CFLAGS="-O3"
+    '';
+in
+  pkgs.stdenv.mkDerivation {
+    name = "cpuminer";
+    src = src;
+    buildInputs = [pkgs.autoconf pkgs.automake pkgs.curl];
+    configurePhase = configurePhase;
+    buildPhase = ''
       make
-   '';
-  installPhase = ''
-     mkdir -p $out/bin
-     cp minerd $out/bin
-   '';
-}
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp minerd $out/bin
+    '';
+  }
--- devenv.nix
+++ devenv.nix
@@ -1,25 +1,32 @@
-{ pkgs, lib, config, inputs, ... }:
-let
-  minerd = import ./cpuminer.nix {pkgs=pkgs;};
-  bitcoind = import ./bitcoind.nix {pkgs=pkgs; lib=lib;};
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  minerd = import ./cpuminer.nix {pkgs = pkgs;};
+  bitcoind = import ./bitcoind.nix {
+    pkgs = pkgs;
+    lib = lib;
+  };
 in {
-
   env.BITCOIND_DATADIR = config.devenv.root + "/.devenv/state/bitcoind";
 
   # https://devenv.sh/packages/
-  packages = [ bitcoind minerd ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ pkgs.darwin.apple_sdk.frameworks.Security ];
+  packages = [bitcoind minerd] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.darwin.apple_sdk.frameworks.Security];
 
   # https://devenv.sh/languages/
   languages.rust.enable = true;
 
   # https://devenv.sh/processes/
   processes = {
-     run-local-pool.exec = "run-local-pool";
-     run-job-server.exec = "run-job-server";
-     run-job-client.exec = "run-job-client";
-     run-translator-proxy.exec = "run-translator-proxy";
-     bitcoind-testnet.exec = "bitcoind-testnet";
-     run-minerd.exec = "run-minerd";
+    run-local-pool.exec = "run-local-pool";
+    run-job-server.exec = "run-job-server";
+    run-job-client.exec = "run-job-client";
+    run-translator-proxy.exec = "run-translator-proxy";
+    bitcoind-testnet.exec = "bitcoind-testnet";
+    run-minerd.exec = "run-minerd";
   };
 
   # https://devenv.sh/basics/
@@ -27,12 +34,12 @@ in {
   # services.postgres.enable = true;
   # https://devenv.sh/scripts/
   scripts = {
-     run-local-pool.exec = "cargo -C roles/pool -Z unstable-options run -- -c $DEVENV_ROOT/roles/pool/config-examples/pool-config-local-tp-example.toml";
-     run-job-server.exec = "cargo -C roles/jd-server -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-server/config-examples/jds-config-local-example.toml";
-     run-job-client.exec = "cargo -C roles/jd-client -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-client/config-examples/jdc-config-local-example.toml";
-     run-translator-proxy.exec = "cargo -C roles/translator -Z unstable-options run -- -c $DEVENV_ROOT/roles/translator/config-examples/tproxy-config-local-jdc-example.toml";
-     bitcoind-testnet.exec = "bitcoind -testnet4 -sv2 -sv2port=8442 -debug=sv2 -conf=$DEVENV_ROOT/bitcoin.conf -datadir=$BITCOIND_DATADIR";
-     run-minerd.exec = "minerd -a sha256d -o stratum+tcp://localhost:34255 -q -D -P";
+    run-local-pool.exec = "cargo -C roles/pool -Z unstable-options run -- -c $DEVENV_ROOT/roles/pool/config-examples/pool-config-local-tp-example.toml";
+    run-job-server.exec = "cargo -C roles/jd-server -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-server/config-examples/jds-config-local-example.toml";
+    run-job-client.exec = "cargo -C roles/jd-client -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-client/config-examples/jdc-config-local-example.toml";
+    run-translator-proxy.exec = "cargo -C roles/translator -Z unstable-options run -- -c $DEVENV_ROOT/roles/translator/config-examples/tproxy-config-local-jdc-example.toml";
+    bitcoind-testnet.exec = "bitcoind -testnet4 -sv2 -sv2port=8442 -debug=sv2 -conf=$DEVENV_ROOT/bitcoin.conf -datadir=$BITCOIND_DATADIR";
+    run-minerd.exec = "minerd -a sha256d -o stratum+tcp://localhost:34255 -q -D -P";
   };
 
   # https://devenv.sh/tasks/
@@ -42,6 +49,10 @@ in {
 
   tasks."bitcoind:make_datadir" = {
     exec = ''mkdir -p $BITCOIND_DATADIR'';
-    before = [ "devenv:enterShell" ];
+    before = ["devenv:enterShell"];
+  };
+
+  pre-commit.hooks = {
+    alejandra.enable = true;
   };
 }
