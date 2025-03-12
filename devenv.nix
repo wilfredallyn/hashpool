0b997281ca45a79c6588662cf8a08ef25c9573bb
vnprc <vnprc@protonmail.com>
2025-03-11 22:08:10 -0400

    ditch cpuminer aka minerd in favor of mining-device-sv1
    The SRI repo already comes with a rust cpu miner
    but the online docs tell you to use minerd instead
    so I didn't even know it existed!
    
    this commit removes minerd and it'd deps and build steps
    and replaces it with the built in cpu miner, mining-device-sv1
    this significantly reduces build and startup time
    
    add a control loop bash script to devenv.nix
    so the miner doesn't start before the proxy and panic

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,6 @@
   inputs,
   ...
 }: let
-  minerd = import ./cpuminer.nix {pkgs = pkgs;};
   bitcoind = import ./bitcoind.nix {
     pkgs = pkgs;
     lib = lib;
@@ -15,7 +14,11 @@ in {
 
   # https://devenv.sh/packages/
   packages =
-    [bitcoind minerd pkgs.just]
+    [
+      pkgs.netcat
+      bitcoind
+      pkgs.just
+    ]
     ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [pkgs.darwin.apple_sdk.frameworks.Security];
 
   # https://devenv.sh/languages/
@@ -28,7 +31,14 @@ in {
     run-job-client.exec = "run-job-client";
     run-translator-proxy.exec = "run-translator-proxy";
     bitcoind-testnet.exec = "bitcoind-testnet";
-    run-minerd.exec = "run-minerd";
+    run-miner.exec = ''
+      echo "Waiting for translator proxy on port 34255..."
+      while ! nc -z localhost 34255; do
+        sleep 1
+      done
+      echo "Translator proxy is up, starting miner..."
+      cd roles/test-utils/mining-device-sv1 && cargo run
+    '';
   };
 
   # https://devenv.sh/basics/
@@ -41,13 +51,7 @@ in {
     run-job-client.exec = "cargo -C roles/jd-client -Z unstable-options run -- -c $DEVENV_ROOT/roles/jd-client/config-examples/jdc-config-local-example.toml";
     run-translator-proxy.exec = "cargo -C roles/translator -Z unstable-options run -- -c $DEVENV_ROOT/roles/translator/config-examples/tproxy-config-local-jdc-example.toml";
     bitcoind-testnet.exec = "bitcoind -testnet4 -sv2 -sv2port=8442 -debug=sv2 -conf=$DEVENV_ROOT/bitcoin.conf -datadir=$BITCOIND_DATADIR";
-    run-minerd.exec = "minerd -a sha256d -o stratum+tcp://localhost:34255 -q -D -P";
   };
-
-  # https://devenv.sh/tasks/
-  # https://devenv.sh/tests/
-  # https://devenv.sh/pre-commit-hooks/
-  # See full reference at https://devenv.sh/reference/options/
 
   tasks."bitcoind:make_datadir" = {
     exec = ''mkdir -p $BITCOIND_DATADIR'';
